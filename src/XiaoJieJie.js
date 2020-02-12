98e614a388418f3a104677a3ad56ba418bf79479
tenshinotsubasa <tenshinotsubasa@users.noreply.github.com>
2020-02-12 17:18:10 +0800

    技术胖(18)setState的异步性

--- src/XiaoJieJie.js
+++ src/XiaoJieJie.js
@@ -23,12 +23,15 @@ class XiaoJieJie extends Component {
                     className="input"
                     value={this.state.inputValue}
                     onChange={this.inputChange.bind(this)}
+
+                    // input绑定
+                    ref = {(input)=>{this.input = input}}
                 >
                 </input>
                 <button onClick={this.onAddServ.bind(this)}>增加服务</button>
             </div>
 
-            <ul>
+            <ul ref={(ul)=>{this.ul=ul}}>
                 {this.state.list.map((item, index) => {
                 return (
                         /* {向子组件传值} */
@@ -47,18 +50,24 @@ class XiaoJieJie extends Component {
     }
 
     inputChange(e) {
-        console.log(e.target.value);
+        // console.log(e.target.value);
         // this.state.inputValue = e.target.value;
         this.setState({
-        inputValue: e.target.value
+            // inputValue: e.target.value
+            inputValue:this.input.value
         });
     }
 
     onAddServ() {
+
+        /// setState是异步执行的
         this.setState({
         list: [...this.state.list, this.state.inputValue],
         inputValue: ""
-        });
+        }, ()=>{console.log(this.ul.querySelectorAll('li').length)});
+
+        /// <这里的执行结果为setstate更新前的值，可在setState的回调中调用
+        // console.log(this.ul.querySelectorAll('li').length)
     }
 
     deleteItem(index) {
