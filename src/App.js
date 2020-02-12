f9fb540c6772ef834a765d6fdb7c7f98d5182d4b
tenshinotsubasa <tenshinotsubasa@users.noreply.github.com>
2020-02-12 12:13:48 +0800

    hello world

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+// import React from 'react';
+// const Component = React.Component;
+
+class App extends Component {
+  render() {
+    // return <div>Hello World!!</div>;
+    return (
+      <ul className="my-list">
+        <li>www.baidu.com</li>
+        <li>I Love React</li>
+      </ul>
+    );
+  }
+}
+
+export default App;
