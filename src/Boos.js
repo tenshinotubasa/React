eb77c10b20307ab246b5499069b51793f1fa853f
tenshinotsubasa <tenshinotsubasa@users.noreply.github.com>
2020-02-13 10:36:16 +0800

    技术胖(27)react-transition-group(CSSTransition组件)

--- src/Boos.js
+++ src/Boos.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import {CSSTransition} from 'react-transition-group'
 class Boss extends Component {
     constructor(props) {
         super(props);
@@ -9,7 +9,14 @@ class Boss extends Component {
     render() { 
         return ( 
             <div>
-                <div className={this.state.isShow ? 'show':'hide'}>Boss级任务-孙悟空</div>
+                <CSSTransition
+                    in={this.state.isShow}
+                    timeout={2000}
+                    classNames="boss-text"
+                    unmountOnExit
+                >
+                    <div>Boss级人物-孙悟空</div>
+                </CSSTransition>
                 <div><button onClick={this.toToggole}>召唤Boss</button></div>
             </div>
          );
